5ccef16ba6735658acc904544bb9491c1e949324
s27051 <s27051@pjwstk.edu.pl>
2024-03-19 22:16:16 +0100

    Added arrays ad pepraration of data

--- Perceptron/main.py
+++ Perceptron/main.py
@@ -5,7 +5,20 @@ import matplotlib.pyplot as plt
 # Insert TrainingFile
 TrainingFile = str(input("Training File: "))
 if TrainingFile == "": TrainingFile = "perceptron.data"
+f1 = open(TrainingFile, "r")
+TraningData = []
+for x in f1.readlines():
+    TraningData.append(x.split(','))
+
+
 
 # Insert Testfile
 TestFile = str(input("Test File: "))
 if TestFile == "": TestFile = "perceptron.test.data"
+f2 = open(TestFile, "r")
+TestData = []
+for x in f2.readlines():
+    TraningData.append(x.split(','))
+
+# Calculate vector length
+vectorLength = len(TraningData[0])-1
