173895623c1a66f1c03577b6ddd8a287728b3569
s27051 <s27051@pjwstk.edu.pl>
2024-03-19 21:31:17 +0100

    Added inserting files variables

--- Perceptron/main.py
+++ Perceptron/main.py
@@ -2,3 +2,10 @@ import fileinput
 import random
 import matplotlib.pyplot as plt
 
+# Insert TrainingFile
+TrainingFile = str(input("Training File: "))
+if TrainingFile == "": TrainingFile = "perceptron.data"
+
+# Insert Testfile
+TestFile = str(input("Test File: "))
+if TestFile == "": TestFile = "perceptron.test.data"
